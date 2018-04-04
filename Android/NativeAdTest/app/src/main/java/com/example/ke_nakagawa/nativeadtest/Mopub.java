ec91dd9f8514c243514f5ef1c82f74dc390089f5
nkws <keigo_nakagawa@voyagegroup.com>
2018-04-04 18:53:14 +0900

    add mopub native ad

---
+++ Android/NativeAdTest/app/src/main/java/com/example/ke_nakagawa/nativeadtest/Mopub.java
@@ -0,0 +1,60 @@
+package com.example.ke_nakagawa.nativeadtest;
+
+import android.app.DownloadManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.mopub.nativeads.MoPubAdAdapter;
+import com.mopub.nativeads.MoPubNativeAdPositioning;
+import com.mopub.nativeads.MoPubStaticNativeAdRenderer;
+import com.mopub.nativeads.RequestParameters;
+import com.mopub.nativeads.ViewBinder;
+
+import java.util.EnumSet;
+
+public class Mopub extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mopub);
+
+        final ListView listView = (ListView) findViewById(R.id.native_list_view);
+
+        final EnumSet<RequestParameters.NativeAdAsset> desiredAssets = EnumSet.of(
+                RequestParameters.NativeAdAsset.TITLE,
+                RequestParameters.NativeAdAsset.TEXT,
+                RequestParameters.NativeAdAsset.ICON_IMAGE,
+                RequestParameters.NativeAdAsset.MAIN_IMAGE,
+                RequestParameters.NativeAdAsset.CALL_TO_ACTION_TEXT);
+
+        RequestParameters requestParameters = new RequestParameters.Builder()
+                .desiredAssets(desiredAssets)
+                .build();
+
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1);
+        for (int i = 0; i < 100; ++i) {
+            adapter.add("Item " + i);
+        }
+
+        MoPubAdAdapter adAdapter = new MoPubAdAdapter(this, adapter, new MoPubNativeAdPositioning.MoPubServerPositioning());
+
+        final MoPubStaticNativeAdRenderer staticNativeAdRenderer = new MoPubStaticNativeAdRenderer(
+                new ViewBinder.Builder(R.layout.native_ad_list_item)
+                    .titleId(R.id.native_title)
+                    .textId(R.id.native_text)
+                    .mainImageId(R.id.native_main_image)
+                    .iconImageId(R.id.native_icon_image)
+                    .callToActionId(R.id.native_cta)
+                    .privacyInformationIconImageId(R.id.native_privacy_information_icon_image)
+                    .build());
+        adAdapter.registerAdRenderer(staticNativeAdRenderer);
+        listView.setAdapter(adAdapter);
+
+        // TODO: set adUnitId
+        adAdapter.loadAds("adUnitId", requestParameters);
+    }
+}
